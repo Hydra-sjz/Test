3a160ff65c937c475493c6bda6d778c3f6678d6f
Hydra <119828208+Hydra-sjz@users.noreply.github.com>
2023-06-07 20:02:13 +0530

    Update callback.py

--- mbot/plugins/callback.py
+++ mbot/plugins/callback.py
@@ -133,7 +133,7 @@ async def get_anime_kitsu_cb(c: Client, cb: CallbackQuery):
     a_id = cb.data.split("_")[1]
     photo, msg = await kitsu_get_anime(a_id)
     if msg:
-        await c.delete_messages(chat_id=cb.message.chat.id, message_ids=[cb.message.message_id])
+        await c.delete_messages(chat_id=cb.message.chat.id, message_ids=[cb.message.id])
         await c.send_photo(
             chat_id=cb.message.chat.id,
             photo=photo,
