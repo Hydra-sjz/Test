dc6d08d431caf50edaff082b2319bef09def7895
Hydra <119828208+Hydra-sjz@users.noreply.github.com>
2023-06-08 00:32:53 +0530

    Update callback.py

--- mbot/plugins/callback.py
+++ mbot/plugins/callback.py
@@ -4,9 +4,6 @@ from mbot import Mbot as app
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 import wget
 import os 
-import requests
-from pyrogram import filters, Client
-from helper.kitsu_api import kitsu_get_anime
 
 
 email=''
@@ -128,20 +125,7 @@ async def back(_, query):
              ]]
              return await query.message.edit(text=string, reply_markup=InlineKeyboardMarkup(buttons))
             
-@app.on_callback_query(filters.regex("k_"))
-async def get_anime_kitsu_cb(c: Client, cb: CallbackQuery):
-    a_id = cb.data.split("_")[1]
-    photo, msg = await kitsu_get_anime(a_id)
-    if msg:
-        await c.delete_messages(chat_id=cb.message.chat.id, message_ids=[cb.message.id])
-        await c.send_photo(
-            chat_id=cb.message.chat.id,
-            photo=photo,
-            caption=msg,
-            reply_to_message.id=cb.message.reply_to_message.message.id
-        ) 
-      
-            
+
             
             
             
