f35f489e6c46f46a3cd26b89463fb43382dbf2b8
Hydra <119828208+Hydra-sjz@users.noreply.github.com>
2023-06-08 19:22:26 +0530

    Update bot.py

--- mbot/plugins/animply/bot.py
+++ mbot/plugins/animply/bot.py
@@ -109,7 +109,7 @@ from mbot.utils2.data_parser import (
     VIEWER_QRY,
     RECOMMENDTIONS_QUERY,
 )
-from mbot.plugins.anibot.anilist import auth_link_cmd, code_cmd, logout_cmd
+from mbot.plugins.animply.anilist import auth_link_cmd, code_cmd, logout_cmd
 
 USERS = get_collection("USERS")
 GROUPS = get_collection("GROUPS")
